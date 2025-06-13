2f0fd99328ed028d9def45be6badda511569472d
Baran Çakı <baran.caki@hotmail.com>
2025-06-14 00:02:56 +0300

    Updated

--- driver/driver_path.py
+++ driver/driver_path.py
@@ -1,6 +1,16 @@
-
+import os
+import platform
 
 def get_the_driver_path():
-    #write your chromedriver path
-    driver_path = "/Users/barancaki/Documents/chromedriver-mac-arm64/chromedriver"
-    return driver_path
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    if platform.system() == "Darwin":
+        # macOS için
+        driver_path = os.path.join(base_dir, "../chromedriver-mac/chromedriver")
+    elif platform.system() == "Windows":
+        # Windows için
+        driver_path = os.path.join(base_dir, "../chromedriver-win/chromedriver.exe")
+    else:
+        raise Exception("Desteklenmeyen işletim sistemi")
+
+    return os.path.abspath(driver_path)
